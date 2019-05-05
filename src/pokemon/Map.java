c0f28df31ae35e8c403965ca9fb9bd0fc2318fde
Gamonga <gabrielprodrigues@usp.br>
2019-05-05 00:42:18 -0300

    Mapa final

--- src/pokemon/Map.java
+++ src/pokemon/Map.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Map {
 	static Scanner scan = new Scanner(System.in);
-	// @formatter:off
-	static final char[][] m_atual = { 
+	static char[][] m_atual = { 
 			{' ',' ',' ',' ',' '},
 			{' ',' ',' ',' ',' '},			
 			{' ',' ',' ',' ',' '},
@@ -13,154 +12,207 @@ public class Map {
 			{' ',' ',' ',' ',' '},
 			};
 	static final char[][] m1 = { 
-			{'u','u','u','u','u'},
-			{'p','*',' ','*','r'},
-			{'p','*',' ','*','r'},
-			{'p','*','T','*','r'},
+			{'p','u','u','u','p'},
+			{'p','*','.','*','r'},
+			{'p','*','.','*','r'},
+			{'p','*','.','*','r'},
 			{'p','p','p','p','p'},
 			};
 	static final char[][] m2 = { 
-			{'u','u','u','u','u'},
-			{'l','*',' ','*','r'},
-			{'l',' ',' ',' ','r'},
+			{'p','u','u','u','p'},
+			{'l','*','.','*','r'},
+			{'l','.','.','.','r'},
 			{'l','*','*','*','r'},
 			{'p','p','p','p','p'},
 			};
 	static final char[][] m3 = { 
-		    {'u','u','u','u','u'},
-			{'l','*','*',' ','p'},
-			{'l',' ',' ',' ','p'},
+		    {'p','u','u','u','p'},
+			{'l','*','*','.','p'},
+			{'l','.','.','.','p'},
 			{'l','*','*','*','p'},
 			{'p','p','p','p','p'},
 			};
 	static final char[][] m4 = { 
-			{'u','u','u','u','u'},
+			{'p','u','u','u','p'},
 			{'p','*','*','*','r'},
-			{'p','*',' ',' ','r'},
-			{'p','*',' ','*','r'},
-			{'d','d','d','d','d'}
+			{'p','*','.','.','r'},
+			{'p','*','.','*','r'},
+			{'d','d','d','d','p'}
 			};
 	static final char[][] m5 = { 
-			{'u','u','u','u','u'},
+			{'p','u','u','u','p'},
 			{'l','*','*','*','r'},
-			{'l',' ',' ','*','r'},
-			{'l','*',' ','*','r'},
-			{'d','d','d','d','d'}
+			{'l','.','.','*','r'},
+			{'l','*','.','*','r'},
+			{'p','d','d','d','p'}
 			};
 	static final char[][] m6 = { 
-			{'u','u','u','u','u'},
-			{'l','*',' ','*','p'},
-			{'l',' ',' ',' ','p'},
-			{'l',' ','*',' ','p'},
-			{'d','d','d','d','d'}
+			{'p','u','u','u','p'},
+			{'l','*','.','*','p'},
+			{'l','.','.','.','p'},
+			{'l','.','*','.','p'},
+			{'p','d','d','d','p'}
 			};
 	static final char[][] m7 = { 
 			{'p','p','p','p','p'},
 			{'p','*','*','*','r'},
 			{'p','*','g','*','r'},
 			{'p','*','*','*','r'},
-			{'d','d','d','d','d'}
+			{'p','d','d','d','p'}
 			};
 	static final char[][] m8 = { 
 			{'p','p','p','p','p'},
 			{'l','*','*','*','r'},
-			{'l','*','i',' ','r'},
+			{'l','*','i','.','r'},
 			{'l','*','*','*','r'},
-			{'d','d','d','d','d'}
+			{'p','d','d','d','p'}
 			};
 	static final char[][] m9 = { 
 			{'p','p','p','p','p'},
 			{'l','*','*','*','p'},
-			{'l',' ',' ','*','p'},
-			{'l','*',' ','*','p'},
-			{'d','d','d','d','d'}
+			{'l','.','.','*','p'},
+			{'l','*','.','*','p'},
+			{'p','d','d','d','p'}
 			};
 	static final char[][][][] mm = {
 			{m7,m8,m9},
 			{m4,m5,m6},
 			{m1,m2,m3}
 	};
-	// @formatter:on
 	public static void imprime(char[][] map) {
 		System.out.println();
-		for (int i = 1; i <= 3; i++) {
+		for(int i=1;i<=3;i++) {
 			System.out.print("        |");
-			for (int j = 1; j <= 3; j++) {
-				System.out.print(map[i][j] + "|");
+			for(int j=1;j<=3;j++)  {
+				System.out.print(map[i][j]+"|");
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
-	
-	public static void copia(char[][] map, char[][] m_atual) {
+	public static void copia(char[][][][] map,char[][] m_atual,int m, int n) {
 		System.out.println();
-		for (int i = 0; i <= 4; i++) {
-			for (int j = 0; j <= 4; j++) {
-				m_atual[i][j] = map[i][j];
+		for(int i=0;i<=4;i++) {
+			for(int j=0;j<=4;j++)  {
+				m_atual[i][j]=map[m][n][i][j];
 			}
 		}
 	}
-	
-	public static String move_map(char[][][][] m_atual, int i, int j, int m, int n, char[][][][] map) {
-		int continua = 0;
+	public static String move_map(char[][] map) {
+		int continua=0,i=3,j=2,m=2,n=0;
 		String move = " ";
-		while (continua == 0) {
+		copia(mm,map,m,n);
+		map[i][j]='T';
+		while(continua==0) {
+			imprime(map);
+			System.out.println("Escolha a sua direcao: ");
 			move = scan.nextLine();
-			continua = 0;
-			switch (move) {
-			case "u":
-				if (verifica(m_atual, i, j + 1, m, n)) {
-					m_atual[m][n][i][j + 1] = m_atual[m][n][i][j];
-					m_atual[m][n][i][j] = map[m][n][i][j];
-					continua = 1;
-				} else
+			switch(move) {
+				case "u":
+					if(verifica(i-1,j,m,n)) {
+						if(mm[m][n][i-1][j]=='p') {
+							System.out.println("entrou p ");
+							continua=0;
+						}
+						else {
+							map[3][j]='T';
+							m--;
+							i=3;
+							continua=0;
+						}
+					}
+					else { 
+						map[i-1][j]=map[i][j];
+						map[i][j]=mm[m][n][i][j];
+						i--;
+						continua=0;				
+					}
 					break;
-			case "r":
-				if (verifica(m_atual, i + 1, j, m, n)) {
-					m_atual[m][n][i + 1][j] = m_atual[m][n][i][j];
-					m_atual[m][n][i][j] = map[m][n][i][j];
-					continua = 1;
-				} else
+				case "r":
+					if(verifica(i,j+1,m,n)) {
+						if(mm[m][n][i][j+1]=='p') {
+							System.out.println("entrou p ");
+							continua=0;
+						}
+						else {
+							map[i][1]='T';
+							n++;
+							j=1;
+							continua=0;
+						}
+					}
+					else {
+						map[i][j+1]=map[i][j];
+						map[i][j]=mm[m][n][i][j];
+						j++;
+						continua=0;
+					}
 					break;
-			case "l":
-				if (verifica(m_atual, i - 1, j, m, n)) {
-					m_atual[m][n][i - 1][j] = m_atual[m][n][i][j];
-					m_atual[m][n][i][j] = map[m][n][i][j];
-					continua = 1;
-				} else
+				case "l":
+					if(verifica(i,j-1,m,n)) {
+						if(mm[m][n][i][j-1]=='p') {
+							System.out.println("entrou p ");
+							continua=0;
+						}
+						else {
+							map[i][3]='T';
+							n--;
+							j=3;
+							continua=0;
+						}
+					}
+					else {
+						map[i][j-1]=map[i][j];
+						map[i][j]=mm[m][n][i][j];
+						j--;
+						continua=0;
+					}
 					break;
-			case "d":
-				if (verifica(m_atual, i, j - 1, m, n)) {
-					m_atual[m][n][i][j - 1] = m_atual[m][n][i][j];
-					m_atual[m][n][i][j] = map[m][n][i][j];
-					continua = 1;
-				} else
+				case "d":
+					if(verifica(i+1,j,m,n)) {
+						if(mm[m][n][i+1][j]=='p') {
+							System.out.println("entrou p ");
+							continua=0;
+						}
+						else {
+							map[1][j]='T';
+							m++;
+							i=1;
+							continua=0;
+						}
+					}
+					else {
+						map[i+1][j]=map[i][j];
+						map[i][j]=mm[m][n][i][j];
+						i++;
+						continua=0;
+					}
 					break;
 			}
 		}
 		return move;
 	}
-	
-	public static boolean verifica(char[][][][] map, int i, int j, int m, int n) {
-		switch (map[m][n][i][j]) {
+	public static boolean verifica(int i, int j,int m,int n) {
+		switch(m_atual[i][j]) {
 		case 'u':
-			map[m][n][i][j] = map[m + 1][n][i - 2][j];
+			copia(mm,m_atual,m-1,n);
+			return true;
 		case 'r':
-			map[m][n][i][j] = map[m][n + 1][i][j - 2];
+			copia(mm,m_atual,m,n+1);
+			return true;
 		case 'l':
-			map[m][n][i][j] = map[m][n - 1][i][j + 2];
+			copia(mm,m_atual,m,n-1);
+			return true;
 		case 'd':
-			map[m][n][i][j] = map[m - 1][n][i + 2][j];
+			copia(mm,m_atual,m+1,n);
+			return true;
 		case 'p':
-			System.out.println("Nao e posivel ir por esse caminho escoha outra opcao");
-			return false;
-		}
-		return true;
+			System.out.println("Nao e posivel ir por esse caminho escolha outra opcao");
+			return true;
 	}
-	
+		return false;
+	}
 	public static void main(String[] args) {
-		copia(m3, m_atual);
-		imprime(m_atual);
+		move_map(m_atual);
 	}
 }
